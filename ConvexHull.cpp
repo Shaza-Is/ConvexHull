c53115070149753b5b557febfa8d2ea440340f34
Shaza-Is <shaza993@yahoo.com>
2017-02-22 17:18:11 +0200

    adding includes

--- ConvexHull.cpp
+++ ConvexHull.cpp
@@ -1,3 +1,9 @@
+// -------------------- C++ 
+#include <iostream>
+// -------------------- OpenMesh
+#include <OpenMesh/Core/IO/MeshIO.hh>
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+typedef OpenMesh::TriMesh_ArrayKernelT<> MyMesh;
 /*reader*/
 
 
