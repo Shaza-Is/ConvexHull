3f5c5cf94fc9d51d5c8367b44fbf7100e182a2d1
Shaza-Is <shaza993@yahoo.com>
2017-03-01 13:47:02 +0200

    testing file

---
+++ ConvexHullTest.cpp
@@ -0,0 +1,31 @@
+// -------------------- C++ 
+#include <iostream>
+#include "ConvexHull.h"
+// -------------------- OpenMesh
+#include <OpenMesh/Core/IO/MeshIO.hh>
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+typedef OpenMesh::TriMesh_ArrayKernelT<> MyMesh;
+
+int main()
+{
+	MyMesh mesh;
+	MyMesh* hull;
+	ConvexHull cHull;
+
+/*reader*/
+	if (!OpenMesh::IO::read_mesh(mesh, "input.off")) 
+	{
+  		std::cerr << "read error\n";
+  		exit(1);
+	}
+
+/*algorithm*/
+	cHull.getHull(&mesh);
+
+/*writer*/
+	if (!OpenMesh::IO::write_mesh(hull, "ConvexHull.off")) 
+	{
+  		std::cerr << "write error\n";
+  		exit(1);
+	}
+}
